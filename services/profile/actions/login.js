8c2d5bdfcb153acd980fda8ce478b72b3836e514
azurebird <white.camilo@gmail.com>
2019-05-05 21:51:16 -0500

    Logs user

--- services/profile/actions/login.js
+++ services/profile/actions/login.js
@@ -5,12 +5,14 @@ export default async function login(req, res) {
     const { email, password } = req.body.credentials;
 
     const profile = await Profile.findOne({ email });
+
     if (profile && !profile.isValidPassword(password)) {
       res.status(400).json({ errors: { global: 'Invalid credentials' } });
     } else if (profile) {
       res.status(200).json({ user: profile.toAuthJSON() });
     }
   } catch (error) {
+    console.log(error);
     res.status(500).json(error);
   }
 }
--- services/profile/routes.js
+++ services/profile/routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import loginProfile from './actions/login';
+import createProfile from './actions/create';
 
 const router = express.Router();
 
+router.post('/', createProfile);
+
 /**
- * Creates a new profile
+ * Tries to log in an user
  */
 router.post('/login', loginProfile);
 
